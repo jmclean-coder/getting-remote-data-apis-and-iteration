ee23fcf0c7e41d344634125084cc52cdf0719110
jmclean-coder <joshmclena@gmail.com>
2020-03-23 17:23:20 -0700

    Completed

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,8 +6,35 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  char_hash = get_char_hash(response_hash, character_name)
+  sorted_array = char_hash["films"].sort
+  info_hash(sorted_array)
+  binding.pry
+end
 
-  # iterate over the response hash to find the collection of `films` for the given
+  def get_char_hash(response_hash, character_name)
+  response_hash["results"].find{|character| character["name"] == character_name}
+  end
+
+  def info_hash(array)
+    film_data_response = array.map{|url| RestClient.get(url)}
+    film_data_aoh = film_data_response.map{|string| JSON.parse(string)}
+  end
+
+
+def print_movies(films)
+  film = films.map{|film_data| film_data["title"]}
+  film.each_with_index{|title, index| puts "#{index+1} #{title}"}
+
+end
+
+def show_character_movies(character)
+  films = get_character_movies_from_api(character)
+  print_movies(films)
+  binding.pry
+end
+
+# iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
@@ -16,16 +43,6 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-end
-
-def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
-end
-
-def show_character_movies(character)
-  films = get_character_movies_from_api(character)
-  print_movies(films)
-end
 
 ## BONUS
 
